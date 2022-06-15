f8f1cda082b38c4500b19c8af72f58ea9b3f8dd5
akashicliq16 <98945027+akashicliq16@users.noreply.github.com>
2022-06-15 10:56:59 +0530

    Qweb (#5)
    * Web_services
    
    * database_connection_webservice
    
    * add_print
    
    * QWeb
    
    * Qweb_inherit
    
    * Customizing Existing Qweb report
    
    * Print_button
    
    * graph/calendar_views
    
    * Demo_data
    
    * Change_Demo_Data

--- hr_company/reports/__init__.py
+++ hr_company/reports/__init__.py
@@ -1,1 +1,3 @@
+# from . import employee_details
 from . import employee_details
+
--- hr_company/reports/employee_details.py
+++ hr_company/reports/employee_details.py
@@ -1,3 +1,7 @@
+from odoo import api, models, _
+
+class EmployeeDetailReport(models.AbstractModel):
+    _name = 'report.hr_company.report_companys'
 # from pyexpat import model
 from odoo import api,fields,models, _
 
@@ -10,9 +14,19 @@ class EmployeeDetailReport(models.Model):
         print("Yes Entered here in the function")
         print("docids",docids)
         docs = self.env['hr_company.hr_company'].browse(docids[0])
+        hrlist=[]
+        for li in docs:
+            vals = {
+                'name':li.name,
+                "phone":li.phone,
+                'city':li.city
+            }
+            hrlist.append(vals)
+            print("list....",hrlist)
         return {
             'doc_model' : 'hr_company.hr_company',
             'data' : data,
             'docs' : docs,
+            'hrlist':hrlist,
             
         }
